5dfa3ebd2edfe5e9f123b354688bfe411ae15b63
SpikeHD <spikegdofficial@gmail.com>
2022-11-11 19:32:24 -0800

    test external html grabbing for pages

--- scripts/window.js
+++ scripts/window.js
@@ -2,5 +2,9 @@
  * Create a window based on name, using HTML in ./windows (which contains the inner HTML of windows to be displayed)
  */
 async function createWindow(name) {
-  console.log(name)
+  const windowManager = document.querySelector('#window_manager')
+  const windowHTML = await fetch('windows/window_base.html')
+  const html = await windowHTML.text()
+
+  windowManager.insertAdjacentHTML('afterbegin', html)
 }
