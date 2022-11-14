8946c2af7813382f296ef5f9eb0bcd8be7f7c3be
SpikeHD <spikegdofficial@gmail.com>
2022-11-14 11:09:38 -0800

    remove windows when bluescreening

--- scripts/experience.js
+++ scripts/experience.js
@@ -3,5 +3,8 @@ async function experience() {
   await bluescreen(7000, 'PROGRAM_ENLIGHTENMENT_EXECUTION_EXCEPTION', '<br/><br/>Your journey to enlightenment has begun.')
   await blackscreen(1000)
 
+  // Close all windows
+  document.querySelectorAll('#window_manager .window').forEach(c => c.remove())
+
   startBoot();
 }
